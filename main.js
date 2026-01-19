704da06407e59dbe9a8796ae39ab1cbc7789beb6
Kajsa Kaker <kk223ic@student.lnu.se>
2026-01-19 22:36:00 +0100

    Lab 1A initial commit with math functions

---
+++ main.js
@@ -0,0 +1,6 @@
+import * as MathUtils from "./math.js";
+
+console.log("Addition: ", MathUtils.add(5, 3));
+console.log("Division: ", MathUtils.divide(20, 3));
+
+console.log("Value of PI: ", MathUtils.PI);
